731f15f3d0934a73022dc2fba4bd3bf3d7807e71
Endrina Toledo <endrx12@gmail.com>
2022-06-16 14:53:32 -0400

    video: 10 alias

---
+++ intro/src/10-alias.ts
@@ -0,0 +1,20 @@
+type UserID = string | boolean | number;
+
+let dynamicVar: UserID = "300";
+
+dynamicVar = true;
+dynamicVar = 200;
+
+
+function helloUser( userId: UserID ) {
+  console.log(`Un saludo al usuario con el número de id ${userId}`);
+}
+
+type Sizes = 'S' | 'M' | 'L' | 'XL';
+
+let shirtSize: Sizes;
+shirtSize = "M";
+
+function yourSize( userSize: Sizes ){
+    console.log(`Tu medida es ${userSize}`);
+}
