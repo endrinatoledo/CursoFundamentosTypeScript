1dd0c29c4e091424a2269ae51fcd2a4e7a11bb7d
Endrina Toledo <endrx12@gmail.com>
2022-06-16 15:58:02 -0400

    video 15: Objects

---
+++ intro/src/15-objts.ts
@@ -0,0 +1,30 @@
+(()=>{
+
+  type Sizes ='S' | 'M' | 'L' | 'XL'
+  type Product ={
+    title: string,
+    createAtt: Date,
+    stock : number,
+    size?: Sizes
+  }
+
+  const product: Product[] = []
+
+  const addProduct = (data:Product)=> {
+    product.push(data)
+  }
+
+  addProduct({
+    title: 'product',
+    createAtt: new Date(1993,1,1) ,
+    stock : 12
+  })
+  addProduct({
+    title: 'product',
+    createAtt: new Date(1993,1,1) ,
+    stock : 12,
+    size : 'L'
+  })
+
+  console.log(product)
+})();
