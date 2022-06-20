072cbbd1ea4e5dd215d8ac91596dc372f6b7389c
Endrina Toledo <37427058+endrinatoledo@users.noreply.github.com>
2022-06-20 16:55:10 -0400

    Video 7: Default

---
+++ intermedio/src/06-default.ts
@@ -0,0 +1,17 @@
+export const createProduct = (
+  id: string | number,
+  isNew: boolean = true,
+  stock : number = 10
+  ) => {
+    return {
+      id,
+      stock,
+      isNew
+    }
+  }
+
+  const p1 = createProduct(1,true,12)
+  console.log(p1)
+
+  const p2 = createProduct(1)
+  console.log(p2)
