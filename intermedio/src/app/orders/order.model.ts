eaf41159c9a098252653ba0bd80a53045af423d8
Endrina Toledo <37427058+endrinatoledo@users.noreply.github.com>
2022-06-27 11:46:50 -0400

    video 13: avance

--- intermedio/src/app/orders/order.model.ts
+++ intermedio/src/app/orders/order.model.ts
@@ -1,9 +1,10 @@
 import {Product} from './../products/product.model'
 import { User } from './../users/user.model'
 
-export interface User {
+export interface Order {
   id : string | number,
   createdSt : Date,
   products : Product[],
+  user: User
 
 }
