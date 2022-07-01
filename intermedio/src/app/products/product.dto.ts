5ff76c7a4df20ef5a34e9dcd4e2a2e2504b2f63f
Endrina Toledo <37427058+endrinatoledo@users.noreply.github.com>
2022-07-01 17:08:19 -0400

    Omit y Pick Type
    Omitir o escoger campos para crear nuevos tipos e interfaces

---
+++ intermedio/src/app/products/product.dto.ts
@@ -0,0 +1,9 @@
+import { Product } from './product.model'
+
+// el omit omite campos
+export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
+  categoryId: string
+}
+
+//el pick escoge campos para crear un tipo nuevo
+type example = Pick<Product, 'color' | 'description'>
--- intermedio/src/app/products/product.service.ts
+++ intermedio/src/app/products/product.service.ts
@@ -1,9 +1,23 @@
+import { faker } from '@faker-js/faker';
 import {Product} from './product.model'
-
+import { CreateProductDto } from './product.dto'
 
 export const products : Product[] =[]
-export const addProduct = (data: Product) =>{
-  products.push(data)
+export const addProduct = (data: CreateProductDto):Product =>{
+  const newProduct ={
+    ...data,
+    id: faker.datatype.uuid(),
+    createdAt: faker.date.recent(),
+    updateAt: faker.date.recent(),
+    category:{
+      id: data.categoryId,
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updateAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct)
+  return newProduct
 }
 
 export const updateProduct = (id: string, change: Product) => {
